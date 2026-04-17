f1f58523f7dc9e2c58fd30f0d4e22903cac57cbd
nuwo <sripriya.dev@gmail.com>
2026-04-17 16:06:13 -0700

    changed normalize.css/normalize.css to normalize.css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import 'normalize.css/normalize.css'
+import 'normalize.css'
 import './App.css'
 
 function App() {
