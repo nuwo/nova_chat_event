ecf18b7a927bf03853674d81d02d8c29ae69a397
nuwo <sripriya.dev@gmail.com>
2026-04-17 16:49:30 -0700

    included index.css in App.jsx and included button[type=submit] in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'normalize.css'
+impirt './index.css'
 import './App.css'
 
 function App() {
